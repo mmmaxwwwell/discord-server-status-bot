6f1be7d3449606e2a395bb3331e97cc77ce297a7
mmmaxwwwell <mmmaxwwwell@github.com>
2020-07-19 00:17:40 -0400

    per room server messages

--- app/main.js
+++ app/main.js
@@ -4,22 +4,44 @@ var shell = require("shelljs");
 shell.config.silent = true;
 const Gamedig = require('gamedig');
 const servers = JSON.parse(process.env.GAMESERVERS_JSON)
+const moment = require('moment')
+const tz = require('moment-timezone')
 const { setIntervalAsync } = require('set-interval-async/legacy')
 
 const buildServerMessage = async (server) => {
   const {id, type, host, port, discordStatusChanelId} = server
-  const query = await Gamedig.query({
-    type,
-    host,
-    port
-  })
+  let query
+  try{
+     query = await Gamedig.query({
+      type,
+      host,
+      port
+    })
 
-  return {discordStatusChanelId, message: `${query.raw.rules.GameName}: ${query.raw.game}
-  Players: ${query.players.length}/${query.maxplayers}
-  Map: ${query.map}
-  Version: ${query.raw.rules.Version}
-  GameMode: ${query.raw.rules.GameMode}
-  steam://connect/${query.raw.rules.IP}:${query.raw.rules.Port}`}
+  }catch(error){
+    console.log(error)
+  }
+  if(!query)
+    return ""
+  switch (type){
+    case '7d2d':
+      return {discordStatusChanelId, message: `${query.raw.rules.GameName}: ${query.raw.game}
+      Players: ${query.players.length}/${query.maxplayers}
+      Map: ${query.map}
+      Version: ${query.raw.rules.Version}
+      GameMode: ${query.raw.rules.GameMode}
+      steam://connect/${query.raw.rules.IP}:${query.raw.rules.Port}`}
+    case 'rust':
+      const wiped = new moment(parseInt(query.raw.tags.split(',').find(x => x.startsWith('born')).replace(/\D/g,''))*1000).tz('America/New_York').format("MMMM Do YYYY, h:mm:ss a z")
+      return {discordStatusChanelId, message: `${query.name}: ${query.raw.game}
+      Players: ${query.players.length}/${query.maxplayers}
+      Map: ${query.map.replace('Map','')}
+      Wiped: ${wiped}
+      World Size: ${parseInt(query.raw.rules['world.size'])/1000}km
+      FPS Average: ${query.raw.rules.fps_avg}
+      steam://connect/${query.connect}`}
+  }
+  
 }
 
 const buildMessage = async () => {
@@ -49,15 +71,18 @@ const buildMessage = async () => {
     mountpoint: diskRaw[6]
   }
 
-  let message = [`Resources:
+  let message = [`:
+Resources:
   Memory: ${((parseInt(rawMemory[1]) * 100) / parseInt(rawMemory[0])).toFixed()}%
     Used:  ${memory.used}gb
     Total: ${memory.total}gb
   Disk: ${disk.usedPercent}
     Used:  ${disk.used}gb
     Free:  ${disk.available}gb
-
-`]
+  
+For more detailed stats on our servers, and quick connect links, check out:
+<#728344359422001202>
+<#728025272250794067>`]
 
   let serverMessages = []
   for (const server of servers)
@@ -80,22 +105,21 @@ const updateLastMessage = async (message, channelId) => {
 
   ${new Date().toISOString()}`
 
-  messageContent = messageContent.replace(process.env.REPLACE_IP, process.env.REPLACE_DOMAIN)
+  messageContent = messageContent.replace(/((:?\d{1,3}\.){3,3}\d{1,3})/g, process.env.REPLACE_DOMAIN)
 
-  if(newMessage){
-    channel.send(messageContent)
-  }else{
-    try{
-      const something = await lastMessage.edit(messageContent)
-    }catch{
-      channel.send(messageContent)
-    }
+  try{
+    if(newMessage)
+      await channel.send(messageContent)
+    else
+      await lastMessage.edit(messageContent)
+  }catch{
+
   }
 }
 
 const updateStatus = async () => {
   const infos = await buildMessage()
-  let mainMessageContent = [infos.message, ...infos.serverMessages.map(x => x.message)].join('') 
+  let mainMessageContent = [infos.message].join('') 
   await updateLastMessage(mainMessageContent, process.env.SERVER_STATUS_CHANNEL_ID)
   for (const gameserver of infos.serverMessages)
     await updateLastMessage(gameserver.message, gameserver.discordStatusChanelId)
